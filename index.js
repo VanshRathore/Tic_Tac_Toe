64f93e209daa7c4a84a309dc7f5363652c454704
Vansh Rathore <73779153+VanshRathore@users.noreply.github.com>
2024-10-01 15:59:26 +0530

    Completed the game

--- index.js
+++ index.js
@@ -21,40 +21,42 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }
 
         chance = !chance;
+    })
 
         function winningCombo(char){
+            let result = document.getElementById("result");
             if(arr[0] == char && arr[1] == char && arr[2]== char){
                 // the 0th row is having char
-                alert(`${char} wins`)
+                result.textContent = `${char} wins`;
             }
             if(arr[3] == char && arr[4] == char && arr[5]== char){
                 // the 2nd row is having char
-                alert(`${char} wins`)
+                result.textContent = `${char} wins`;
             }
             if(arr[6] == char && arr[7] == char && arr[8]== char){
                 // the 3rd row is having char
-                alert(`${char} wins`)
+                result.textContent = `${char} wins`;
             }
             if(arr[0] == char && arr[3] == char && arr[6]== char){
                 // the 1st col is having char
-                alert(`${char} wins`)
+                result.textContent = `${char} wins`;
             }
             if(arr[1] == char && arr[4] == char && arr[7]== char){
                 // the 2nd col is having char
-                alert(`${char} wins`)
+                result.textContent = `${char} wins`;
             }
             if(arr[2] == char && arr[5] == char && arr[8]== char){
                 // the 3rd col is having char
-                alert(`${char} wins`)
+                result.textContent = `${char} wins`;
             }
             if(arr[0] == char && arr[4] == char && arr[8]== char){
                 // the 1st diag is having char
-                alert(`${char} wins`)
+                result.textContent = `${char} wins`;
             }
             if(arr[2] == char && arr[4] == char && arr[6]== char){
                 // the 2nd diag is having char
-                alert(`${char} wins`)
+                result.textContent = `${char} wins`;
             }
         }
-    })
+    
 })
